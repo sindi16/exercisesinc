e57a4c49add8a42ac1ba0d7fc6d0330e3cfb39df
sindi16 <10029@holbertonstudents.com>
2024-11-04 06:36:55 +0000

    arrays in c

--- ARRAYS/arrays2.c
+++ ARRAYS/arrays2.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 
-//some examples in c programming for arrays.
-//To print an element of array.
+//
 int main(void)
 {
-    int myage[] = {25 , 35 ,47, 10, 5}; //We have now created a variable that holds an array of four integers.
-    printf("%d\n", myage[2]); //To access an array element, refer to its index number.
+    int myNumbers[] = {3, 50, 75, 100};
+    myNumbers[0] = 25;
+
+    printf("%d", myNumbers[0]);
+
+    // Now outputs 25 instead of 3.
+   
     return (0);
 }
